cf0c3ddf7e32312f1153c41189e950e91ddfd68d
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-10-22 22:23:26 +0000

    :heavy_plus_sign: Adicionando a dependencia do flyway

---
+++ algalog-api/src/main/resources/db/migration/V001__cria-tabela-clientes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cliente (
+   id BIGINT NOT NULL AUTO_INCREMENT,
+   nome VARCHAR(100) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   telefone VARCHAR(20) NOT NULL,
+
+   PRIMARY KEY(id)
+);
