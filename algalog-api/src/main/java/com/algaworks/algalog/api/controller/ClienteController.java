b2848c4e6403b60dba4c327f35222762707ee45f
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-10-23 19:44:40 +0000

    :recycle: Refatorando a classe ClienteController para utilizar o EntityManager

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import com.algaworks.algalog.domain.model.Cliente;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController {
 
+   @PersistenceContext
+   private EntityManager manager;
+
    @GetMapping("/clientes")
    public List<Cliente> listar() {
-
-      Cliente cliente1 = new Cliente();
-      cliente1.setId(1L);
-      cliente1.setNome("João");
-      cliente1.setTelefone("(47) 99999-1111");
-      cliente1.setEmail("joao.sousa@contato.com");
-
-      Cliente cliente2 = new Cliente();
-      cliente2.setId(2L);
-      cliente2.setNome("Maria");
-      cliente2.setTelefone("(47) 98888-4444");
-      cliente2.setEmail("maria.silva@contato.com");
-
-      return Arrays.asList(cliente1, cliente2);
+      return manager.createQuery("from Cliente", Cliente.class).getResultList();
    }
 }
