b67105edfdc0d389be03ca91a5da5b8e35778cf3
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-10-25 18:05:11 +0000

    :sparkles: Impplementando o métoodo de atualizar no ClienteController

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -6,25 +6,32 @@ import java.util.Optional;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
    private ClienteRepository clienteRepository;
 
-   @GetMapping("/clientes")
+   @GetMapping
    public List<Cliente> listar() {
       return clienteRepository.findAll();
    }
 
-   @GetMapping("/clientes/{clienteId}")
+   @GetMapping("/{clienteId}")
    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
       Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 
@@ -33,6 +40,24 @@ public class ClienteController {
       }
 
       return ResponseEntity.notFound().build();
+   }
 
+   @PostMapping
+   @ResponseStatus(HttpStatus.CREATED)
+   public Cliente adicionar(@RequestBody Cliente cliente) {
+      return clienteRepository.save(cliente);
+   }
+
+   @PutMapping("/{clienteId}")
+   public ResponseEntity<Cliente> atualizar(@RequestBody Cliente cliente, @PathVariable Long clienteId) {
+
+      if (!clienteRepository.existsById(clienteId)) {
+         return ResponseEntity.notFound().build();
+      }
+
+      cliente.setId(clienteId);
+      cliente = clienteRepository.save(cliente);
+
+      return ResponseEntity.ok(cliente);
    }
 }
